89056dd1ea329851caa5510be93acb05c8e454a5
Peter <paspiazusabando@gmail.com>
2022-11-02 16:05:38 -0500

    agregando imagenes responsive en home

--- components/slides/GroupTwoImages.tsx
+++ components/slides/GroupTwoImages.tsx
@@ -2,17 +2,25 @@ import {FC} from 'react'
 import Image from 'next/image';
 import { Box } from '@mui/material';
 
+import BannerSlideHomeImgxs from '../../public/images/banner-slide-home-img2-xs.webp'
 import BannerSlideHomeImgsm from '../../public/images/banner-slide-home-img2-sm.webp'
+import BannerSlideHomeImgmd from '../../public/images/banner-slide-home-img2-md.webp'
 import BannerSlideHomeImglg from '../../public/images/banner-slide-home-img2-lg.webp'
 import BannerSlideHomeImgxl from '../../public/images/banner-slide-home-img2-xl.webp'
 
 export const GroupTwoImages: FC = () => {
   return (
     <>
-        <Box sx={{display: {xs: 'block', md: 'none'}}}>
+        <Box sx={{display: {xs: 'block', sm: 'none'}}}>
+            <Image src={BannerSlideHomeImgxs} />
+        </Box>
+        <Box sx={{display: {xs: 'none', sm: 'block', md:'none'}}}>
             <Image src={BannerSlideHomeImgsm} />
         </Box>
-        <Box sx={{display: {xs: 'none', md: 'block', xl: 'none'}}}>
+        <Box sx={{display: {xs: 'none', md: 'block', lg:'none'}}}>
+            <Image src={BannerSlideHomeImgmd} />
+        </Box>
+        <Box sx={{display: {xs: 'none', lg: 'block', xl:'none'}}}>
             <Image src={BannerSlideHomeImglg} />
         </Box>
         <Box sx={{display: {xs: 'none', xl: 'block'}}}>
